a448b0cbfee8351b7febbbed2237045e999348f0
David Luco <dluco11@gmail.com>
2017-03-22 21:35:57 -0700

    Allow saving of persistent cookies.
    Cookies are saved in a local SQLite file in the Mozilla format.
    
    Originally, the cookie code was placed in browser_app_init(), but this
    caused many errors in the program relating the a Visual object? Anyways,
    the correct place to put initialization code is in the startup function
    of the app. startup is only called once when the application is first
    started.

--- browser-app.c
+++ browser-app.c
@@ -1,6 +1,7 @@
 #include "browser-app.h"
 #include "browser-window.h"
 #include <gtk/gtk.h>
+#include <webkit2/webkit2.h>
 
 struct _BrowserApp
 {
@@ -10,11 +11,26 @@ struct _BrowserApp
 G_DEFINE_TYPE(BrowserApp, browser_app, GTK_TYPE_APPLICATION)
 
 static void
-browser_app_activate(GApplication *g_app)
+browser_app_startup(GApplication *g_application)
 {
+	WebKitWebContext *web_context;
+	WebKitCookieManager *cookie_manager;
+
+	G_APPLICATION_CLASS(browser_app_parent_class)->startup(g_application);
+
+	web_context = webkit_web_context_get_default();
+	cookie_manager = webkit_web_context_get_cookie_manager(web_context);
+
+	webkit_cookie_manager_set_persistent_storage(cookie_manager, "cookies.sqlite", WEBKIT_COOKIE_PERSISTENT_STORAGE_SQLITE);
+}
+
+static void
+browser_app_activate(GApplication *g_application)
+{
+	BrowserApp *app = BROWSER_APP(g_application);
 	BrowserWindow *window;
 
-	window = browser_window_new(BROWSER_APP(g_app));
+	window = browser_window_new(app);
 
 	browser_window_open(window, "http://www.google.ca");
 
@@ -22,17 +38,18 @@ browser_app_activate(GApplication *g_app)
 }
 
 static void
-browser_app_open(GApplication *g_app, GFile **files, gint n_files, const gchar *hint)
+browser_app_open(GApplication *g_application, GFile **files, gint n_files, const gchar *hint)
 {
+	BrowserApp *app = BROWSER_APP(g_application);
 	GList *windows;
 	BrowserWindow *window;
 //	int i;
 
-	windows = gtk_application_get_windows(GTK_APPLICATION(g_app));
+	windows = gtk_application_get_windows(GTK_APPLICATION(g_application));
 	if (windows)
 		window = BROWSER_WINDOW(windows->data);
 	else
-		window = browser_window_new(BROWSER_APP(g_app));
+		window = browser_window_new(app);
 
 //	for (i = 0; i < n_files; i++)
 //		browser_window_open(window, files[i]);
@@ -45,6 +62,7 @@ browser_app_class_init(BrowserAppClass *class)
 {
 	GApplicationClass *app_class = G_APPLICATION_CLASS(class);
 
+	app_class->startup = browser_app_startup;
 	app_class->activate = browser_app_activate;
 	app_class->open = browser_app_open;
 }
